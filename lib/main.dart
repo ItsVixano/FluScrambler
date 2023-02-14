dc77b1d86047320ba4552dce354cc3f975c11553
Giovanni Ricca <giovanniricca@proton.me>
2023-02-14 22:23:52 +0100

    FluScrambler: Drop unwanted if checks

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'scrambler.dart';
 
 void main() => runApp(const MyApp());
 
-Color brandColor = const Color(0xFF6750A4); // Main app theme
+Color brandColor = const Color.fromRGBO(103, 80, 164, 1); // Main app theme
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,34 +15,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
-        builder: (ColorScheme? lightDynamic, ColorScheme? dark) {
-      ColorScheme lightColorScheme;
-      ColorScheme darkColorScheme;
+      builder: (light, dark) {
+        final lightColorScheme =
+            light?.harmonized() ?? ColorScheme.fromSeed(seedColor: brandColor);
+        final darkColorScheme = dark?.harmonized() ??
+            ColorScheme.fromSeed(
+                seedColor: brandColor, brightness: Brightness.dark);
 
-      // Android 12 chekcs
-      if (lightDynamic != null && dark != null) {
-        // Use Material You (Monet, Md3) color scheme
-        lightColorScheme = lightDynamic.harmonized();
-        darkColorScheme = dark.harmonized();
-      } else {
-        // Use the main color scheme theme
-        lightColorScheme = ColorScheme.fromSeed(seedColor: brandColor);
-        darkColorScheme = ColorScheme.fromSeed(
-            seedColor: brandColor, brightness: Brightness.dark);
-      }
-      return MaterialApp(
-        title: 'FluScrambler',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: lightColorScheme,
-        ),
-        darkTheme: ThemeData(
-          useMaterial3: true,
-          colorScheme: darkColorScheme,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: const Scrambler(),
-      );
-    });
+        return MaterialApp(
+          title: 'FluScrambler',
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightColorScheme,
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkColorScheme,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const Scrambler(),
+        );
+      },
+    );
   }
 }
